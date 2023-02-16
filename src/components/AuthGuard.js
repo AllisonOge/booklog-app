1469c4cc50d0b81570ca9f50abf3bd13acc572ea
Allison Ogechukwu <ogechukwukanu@gmail.com>
2023-02-16 15:44:51 +0100

    fixed navigation error

--- src/components/AuthGuard.js
+++ src/components/AuthGuard.js
@@ -2,9 +2,10 @@ import { Navigate, Outlet } from "react-router-dom";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "./../firebase-config";
 
-const AuthGuard = () => {
+const AuthGuard = ({ children }) => {
   const [user, loading, error] = useAuthState(auth);
-  return user ? <Outlet /> : <Navigate to="/signin" replace />;
+  
+  return user ? children : <Navigate to="/signin" replace />;
 };
 
 export default AuthGuard;
