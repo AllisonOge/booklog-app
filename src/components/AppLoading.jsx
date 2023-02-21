63e93600f12fd0fab1f9d504d129e3b698bea212
Allison Ogechukwu <ogechukwukanu@gmail.com>
2023-02-21 01:08:02 +0100

    added AppLoading component

---
+++ src/components/AppLoading.jsx
@@ -0,0 +1,9 @@
+const AppLoading = () => {
+  return (
+    <div className="d-flex flex-column justify-content-center align-items-center bg-dark text-white vh-100">
+      Booklog loading ...
+    </div>
+  );
+};
+
+export default AppLoading;
