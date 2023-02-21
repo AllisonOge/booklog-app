33250ffc4b3e5e9647e368ab479de522d8a2f731
Allison Ogechukwu <ogechukwukanu@gmail.com>
2023-02-21 01:05:34 +0100

    renamed AuthGuard component

--- src/components/AuthGuard.jsx
+++ src/components/AuthGuard.jsx
@@ -1,11 +1,18 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useAuthState } from "react-firebase-hooks/auth";
-import { auth } from "./../firebase-config";
+import { auth } from "../firebase-config";
+import AppLoading from "./AppLoading";
 
 const AuthGuard = ({ children }) => {
   const [user, loading, error] = useAuthState(auth);
-  
-  return user ? children : <Navigate to="/signin" replace />;
+
+  return loading ? (
+    <AppLoading />
+  ) : user ? (
+    children
+  ) : (
+    <Navigate to="/signin" replace />
+  );
 };
 
 export default AuthGuard;
